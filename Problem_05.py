066576259a4006ff7d0967f213270d696cbb985e
Shravan Goyal <shravan.goyal@avantika.edu.in>
2026-03-28 15:20:27 +0530

    Update Problem_05.py

--- Problem_05.py
+++ Problem_05.py
@@ -1,1 +1,18 @@
 
+
+
+def calculate_fine(bt , do , dr = 5.0 , mf = 150.0):
+    fine = do * dr
+    if fine > mf:
+        fine = mf
+    return fine
+
+bt = input()
+days = int(input())
+rate = float(input())
+mf = float(input())
+fine = calculate_fine(bt , days , rate , mf)
+
+print(f"Book: {bt}")
+print(f"Days overdue: {days}")
+print(f"Fine: Rs. {fine}")
